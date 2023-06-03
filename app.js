8bea1b60ba37e043dc1c0533e72635fbde7aef33
go2Cshop <shell800808@gmail.com>
2023-06-03 17:42:06 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log("mongodb error!")
+})
+
+db.once('open', () => {
+  console.log("mongodb connected!")
+})
 
 app.get('/', (req, res) => {
   res.send("hello world")

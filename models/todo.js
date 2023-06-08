42ebf159ff26361aa19eefdda9509b0c3d42f5b5
go2Cshop <shell800808@gmail.com>
2023-06-08 12:20:38 +0800

    feat:model todo add isDone params

--- models/todo.js
+++ models/todo.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-
 const todoSchema = new Schema({
   name: {
     type: String,
     required: true
   },
-  done: {
-    type: Boolean
+  isDone: {
+    type: Boolean,
+    default: false
   }
 })
 

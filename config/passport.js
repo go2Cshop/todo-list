c8f595d6e7ae8849128be9b81132fc0d2087bb6a
go2Cshop <shell800808@gmail.com>
2023-07-11 23:25:45 +0800

    feat: add relations between User and Todo models

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ module.exports = app => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, flase, { message: 'That email is not registered!' })
+          return done(null, false, { message: 'That email is not registered!' })
         }
         if (user.password !== password) {
           return done(null, false, { message: 'Email or Password incorrect.' })

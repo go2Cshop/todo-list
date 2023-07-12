c90b18aa4411411e4ec3d77f82786f2310968fae
go2Cshop <shell800808@gmail.com>
2023-07-12 17:06:16 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')//引用提示套件
+//非正式上線模式
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 
@@ -12,13 +16,13 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultlayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const auth = require('./modules/auth')   // 引用模組
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 // 將網址結構符合 / 字串的 request 導向模組 
 router.use('/todos', authenticator, todos)// 加入驗證程序
 router.use('/users', users)
+router.use('/auth', auth)  // 掛載模組
 router.use('/', authenticator, home)//定義寬鬆的路由引到清單最下方，避免攔截到其他的路由。
 
 module.exports = router
